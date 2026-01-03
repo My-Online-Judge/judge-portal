2eb13bf65e94a4bd30de85cca816e93b1c1a71d1
lethanhtuan939 <thanhtuanle939@gmail.com>
2026-01-03 18:42:06 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-6">
+    <Card>
+      <CardHeader>
+        <CardTitle>Hello shadcn-vue!</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Button variant="default" @click="increment" class="bg-blue-500 hover:bg-blue-600">Click me</Button>
+      </CardContent>
+    </Card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+
+watch(count, (newCount) => {
+  console.log(newCount)
+})
+</script>
